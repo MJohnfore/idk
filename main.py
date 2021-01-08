fd52b3c999c477f00e9719bd18056712e30fe6b6
MJohnfore <77157846+MJohnfore@users.noreply.github.com>
2021-01-08 09:25:40 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+num = 9
+start = False
+
+def on_button_pressed_a():
+    global start
+    start = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global start
+    start = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def on_forever():
-    pass
+    if start:
+        global num
+        basic.show_number(num)
+        basic.pause(500)
+        num -= 1
+        if num = 0:
+            basic.show_string("DONE")
 basic.forever(on_forever)
+
+
--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let num = 9
+let start = false
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    start = true
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    start = false
+})
 basic.forever(function on_forever() {
+    if (start) {
+        
+        basic.showNumber(num)
+        basic.pause(500)
+        num -= 1
+    }
     
 })
